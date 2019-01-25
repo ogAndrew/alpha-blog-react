13f019c6074f866da860c7f6f8474242342276dc
Andrew's Rails Course <talle.andrew@gmail.com>
2019-01-24 19:47:05 -0500

    add article component

---
+++ app/javascript/components/Article.js
@@ -0,0 +1,22 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Article extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div class="article-title">
+          <a href={this.props.path}>{this.props.title}</a>
+        </div>
+        <div className="article body">
+          {this.props.description}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+Article.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string
+};
+export default Article
